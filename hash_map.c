122b36b4ea33f765aeaeaae3e195f48b5927fc8f
Henrik Soderhielm <henriksoderhielm@hotmail.com>
2022-12-27 13:47:11 +0100

    added hash table initialiser + operational functions

--- hash_map.c
+++ hash_map.c
@@ -6,10 +6,12 @@
 /* Functionality
 
 Functions:
-	- char ** HashMap() : initialise the hash map
-	- void HashMapAdd(char[] key, char[] value) : add new item to the hashmap
-	- void HashMapDelete(char[] key)
-	- char ** HashMapValues() : get list of all values stored in the HashMap
+	- CharNum(char character) : returns the integer value of a character
+	- Hash(char * string) : returns a integer value for a given string
+	- HashTable() : initialises the hash map, returns map pointer
+	- h_set(h_table * table, char * key, char * value) : add new item to the hashmap
+	- h_delete(char * key) : delete key from hashmap, accounts for linked list
+
 
 Changelog
 	23.12.2022
@@ -18,14 +20,27 @@ Changelog
 		- Completed rolling hash function:
 			- Using Rabin-Karp Search Algorithm
 		- Started work with table structs
+	27.12.2022
+		- Finalised type system for string hashing function
+		- Completed table + entry structs with LinkedList implementation
+		- Added function HashTable()
+		- Added function h_set(h_table * table, char * key, char * value)
+		- Added function h_delete(h_table * table, char * key)
 */
 
-// typedef struct ht {
-// 	entries ** entries;
-// };
+typedef struct h_table {
+	struct entry ** entries;
+} h_table;
 
 
-int power(int base, int exp) {
+typedef struct entry {
+	char * key;
+	char * value;
+	struct node * next;
+} entry;
+
+
+unsigned long long int power(int base, int exp) {
 	unsigned long long int product = base;
 	if (exp == 0) {
 		return 1;
@@ -54,18 +69,72 @@ int Hash(char * string) {
 		sum += char_num * power(base, str_len - i);
 	}
 	int index = sum % TABLE_INT_SIZE;
-	printf("Index: %i\n", index);
-	return 0;
+	// printf("Index: %i\n", index);
+	return index;
+}
+
+
+h_table * HashTable() {
+
+	h_table * new_table = (h_table *) malloc(sizeof(struct h_table));
+	new_table -> entries = malloc(sizeof(struct entry *) * TABLE_INT_SIZE);
+
+	// Set each item in the entries array to zero
+	for (int i = 0; i < TABLE_INT_SIZE; ++i) {
+		new_table -> entries[i] = NULL;
+	}
+
+	return new_table;
+}
+
+
+void h_set(h_table * table, char * key, char * value) {
+
+	int slot = Hash(key); // this is the index in which we place the information
+
+	if (table -> entries[slot] == NULL) {
+		// this means the slot is unoccupied
+		entry * new_entry = NULL;
+		new_entry = (entry *) malloc(sizeof(struct entry));
+		new_entry -> key = key;
+		new_entry -> value = value;
+		new_entry -> next = NULL;
+
+		table -> entries[slot] = new_entry;
+	}
+
+	entry * new_entry = NULL;
+	new_entry = malloc(sizeof(struct entry));
+
+}
+
+void h_delete(h_table * table, char * key) {
+	// deletes a key from the table
+	int slot = Hash(key);
+	if (table -> entries[slot] != NULL) {
+		free(table -> entries[slot]);
+	}
 }
 
 
 void RunTests() {
 
 	printf("Running hash function test...\n");
-	Hash("F7LAKBMANS");
-	Hash("L25I5OWMK5");
-	Hash("6UJN5UV9YO");
-	Hash("PY49WSG5LM");
+	int slot1 = Hash("F7LAKBMANS");
+	int slot2 = Hash("L25I5OWMK5");
+	int slot3 = Hash("6UJN5UV9YO");
+	int slot4 = Hash("PY49WSG5LM");
+	int slot5 = Hash("Helloworld");
+	int slot6 = Hash("foo_bar");
+	int slot7 = Hash("doo_bar");
+
+	printf("Index: %i\n", slot1);
+	printf("Index: %i\n", slot2);
+	printf("Index: %i\n", slot3);
+	printf("Index: %i\n", slot4);
+	printf("Index: %i\n", slot5);
+	printf("Index: %i\n", slot6);
+	printf("Index: %i\n", slot7);
 }
 
 
